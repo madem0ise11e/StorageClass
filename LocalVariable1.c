4ef9ae49bda4e845109899ee933f52b56efe1625
Tetiana Kizan <t.kizan@studenti.unina.it>
2019-12-30 18:27:17 +0100

    Create LocalVariable1.c

---
+++ LocalVariable1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (void)
+{
+  for (int i = 0; i < 5; ++i)
+  { printf("C programming"); }
+  //Error: i is not declared at this point
+  printf("%d", i);
+  return 0;
+}
